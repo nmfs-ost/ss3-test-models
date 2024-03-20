5462d2c8925af9ef1e97fc24ef51a83eafbc8a30
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2024-03-20 14:00:54 +0000

    change, onCreateCommand doesn't seem to work

--- .devcontainer/ss3-exe-download.sh
+++ .devcontainer/ss3-exe-download.sh
@@ -1,2 +1,6 @@
+sudo apt-get update
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt upgrade libstdc++6
+
 wget -O ss3 https://github.com/nmfs-stock-synthesis/stock-synthesis/releases/latest/download/ss3_linux
 sudo chmod a+x ss3
