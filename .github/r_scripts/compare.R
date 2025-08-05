fcdb9fa44cc61fac5c4dd7b6356688702b0312b2
e-perl-NOAA <elizabeth.gugliotti@noaa.gov>
2025-08-05 15:33:44 -0400

    go back

--- .github/r_scripts/compare.R
+++ .github/r_scripts/compare.R
@@ -467,11 +467,9 @@ compare_ss_runs <- function(
   )
   compare_df <- rbind(compare_df, tmp_df)
   compare_df$mod_name <- mod_name # add the model name
-  # only keep rows with ratio != 1 and !is.na for writing to fail_file
+  # only keep rows with !is.na for writing to fail_file
   fail_rows <- compare_df$ratio != 1
 
-  compare_df <- compare_df[round(compare_df$ratio, 6) != 1, ]
-  compare_df <- compare_df[!(compare_df$value == 0 & compare_df$ref_value == 0), ]
   compare_df <- compare_df[complete.cases(compare_df[, c("quantity", "ref_value")]), ]
 
   # print the msg
@@ -481,8 +479,6 @@ compare_ss_runs <- function(
     nsmall = 3,
     justify = "left"
   )
-
-  print(compare_df_print)
 
   # message("values and their differences:")
   if (write_fail == TRUE && any(fail_rows)) {
